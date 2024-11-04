ce899fc3cacdfc35ddf2037f58dbc6ed41724d26
mrgoatking <mrgoatking5@gmail.com>
2024-11-04 17:35:46 +0300

    script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /*==typed js ==*/
 
 const typed = new  Typed('.multiple-text', {
-    strings: [ "Frontend Developer", "Web Developer", "Web Designer", "Freelancer", "Youtuber", "Gamer", "Blogger", "Software Developer", ],
+    strings: [ "Frontend Developer", "Web Developer", "Web Designer", "Freelancer", "Youtuber", "Gamer", "Blogger" ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay:1000,
